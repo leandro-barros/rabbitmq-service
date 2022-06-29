9134b3410f1c3f33305203eb1ab86690f3f9244e
Leandro Barros <leandroedbarros@gmail.com>
2022-06-28 22:48:18 -0300

    Deleting unused codes

--- src/main/java/com/leandro/rabbitmq/Sender.java
+++ src/main/java/com/leandro/rabbitmq/Sender.java
@@ -10,11 +10,6 @@ public class Sender {
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
-//        factory.setUsername("rmlkqywa:rmlkqywa");
-//        factory.setPassword("UCjDND1gVH5rRZGIjVvtvaxv5BwhZwsY");
-//        factory.setHost("beaver.rmq.cloudamqp.com");
-//        factory.setHost("rmlkqywa");
-//        factory.setPort(1883);
 
         factory.setUri("amqps://rmlkqywa:UCjDND1gVH5rRZGIjVvtvaxv5BwhZwsY@beaver.rmq.cloudamqp.com/rmlkqywa");
 
@@ -24,7 +19,7 @@ public class Sender {
 
             channel.queueDeclare(NAME_QUEUE, false, false, false, null);
 
-            String message = "Hello";
+            String message = "Hello World";
 
             channel.basicPublish("", NAME_QUEUE, null, message.getBytes());
         }
