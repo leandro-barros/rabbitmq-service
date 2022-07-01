d1c38ba8d7856bee1340fb86e1f1d56e9b9478d2
Leandro Barros <leandroedbarros@gmail.com>
2022-07-01 15:34:34 -0300

    Feature: Creating exchange topic to send in the messages

---
+++ src/main/java/com/leandro/rabbitmq/exchangetopic/SenderTopic.java
@@ -0,0 +1,34 @@
+package com.leandro.rabbitmq.exchangetopic;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class SenderTopic {
+
+    private static final String NAME_EXCHANGE = "topicExchange";
+
+    private static final String ROUTING_KEY_TOPIC = "quick.orange.rabbit";
+
+    private static final String SECOND_ROUTING_KEY = "secondRoutingKeyTest";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setUri("amqps://rmlkqywa:UCjDND1gVH5rRZGIjVvtvaxv5BwhZwsY@beaver.rmq.cloudamqp.com/rmlkqywa");
+
+        try(Connection connection = factory.newConnection()) {
+
+            Channel channel = connection.createChannel();
+
+            channel.exchangeDeclare(NAME_EXCHANGE, BuiltinExchangeType.TOPIC);
+
+            String message = "Message published in the type Topic";
+
+            channel.basicPublish(NAME_EXCHANGE, ROUTING_KEY_TOPIC, null, message.getBytes());
+
+        }
+
+    }
+}
